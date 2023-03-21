ac2fa6b5a38c7ed04b1950a234318fa2bb513029
dungminhnguyen257 <nguy7670@mylaurier.ca>
2023-03-20 23:23:07 -0400

    complete setting up tailwind and react

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import "./App.css";
-// import { BrowserRouter as Router, Ruotes, Route } from "react-router-dom";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import { Navbar } from "./components/navbar";
 
